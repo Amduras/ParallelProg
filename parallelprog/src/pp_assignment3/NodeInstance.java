4bba15f072af1da5e1d7036e92fd600fce538581
Amduras <sip90@web.de>
2019-09-20 12:57:26 +0200

    wakeup mit 1Grad ausnahme

--- parallelprog/src/pp_assignment3/NodeInstance.java
+++ parallelprog/src/pp_assignment3/NodeInstance.java
@@ -4,8 +4,8 @@ public class NodeInstance extends NodeAbstract implements Node {
 
 	private boolean isAwake = false;
 	private int messages = 0;
-	private Node father;
-	
+	private Node father = null;
+
 	public NodeInstance(String name, boolean initiator) {
 		super(name, initiator);
 	}
@@ -13,29 +13,38 @@ public class NodeInstance extends NodeAbstract implements Node {
 	@Override
 	public void hello(Node neighbour) {
 		if(!this.neighbours.contains(neighbour)) {
-            this.neighbours.add(neighbour);
+			this.neighbours.add(neighbour);
 		}		
 	}
 
 	@Override
 	public void wakeup(Node neighbour) {
-		father = neighbour;
-		
+		if(father == null) {
+			father = neighbour;
+			++messages;
+		}
+		if(this.neighbours.size() == 1) {
+			father.echo(this, null);
+		} else {
+			for(Node i: neighbours) {
+				i.wakeup(this);
+			}
+		}
 	}
 
 	@Override
 	public void echo(Node neighbour, Object data) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setupNeighbours(Node... neighbours) {
 		for(Node i: neighbours) {
-            if(!this.neighbours.contains(i)) {
-                this.neighbours.add(i);                
-            }
-            i.hello(this);
+			if(!this.neighbours.contains(i)) {
+				this.neighbours.add(i);                
+			}
+			i.hello(this);
 		}
 	}
 }
