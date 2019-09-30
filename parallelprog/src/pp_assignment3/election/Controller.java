c13fbd3cadaed73cf3273501a58224d3ff9f4e7e
Jan-Luca Pankowsky <jan.pankowsky@t-online.de>
2019-09-30 21:34:33 +0200

    election rdy?

--- parallelprog/src/pp_assignment3/election/Controller.java
+++ parallelprog/src/pp_assignment3/election/Controller.java
@@ -1,6 +1,11 @@
 package pp_assignment3.election;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 
 
 public class Controller {
@@ -10,20 +15,38 @@ public class Controller {
 	static int Min = 1, Max = 20;
 	static NodeInstance[] nodes;
 	static boolean fin = true;
+	static boolean running = false;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//int initiator = ORANGE;
+	public static void main(String[] args) {		
+		//initialisation
 		int length = 9;
+		int runs = 1;
+		int run = 0;
+		
 		Integer[] id = createId(length);
 		nodes = createTestTree(length, id);
-		System.out.println("Red: "+id[RED]+" Brown: "+id[BROWN]);
-		nodes[RED].setInitiator(id[RED]);
-		nodes[BROWN].setInitiator(id[BROWN]);
-		new Thread(()->nodes[RED].wakeup(nodes[RED],id[RED])).start();
-		new Thread(()->nodes[BROWN].wakeup(nodes[BROWN],id[BROWN])).start();
-		//for(NodeInstance node: nodes)
-		//	System.out.println(node.children.toString());
+		
+		//main loop
+		while(run < runs) {
+			if (running) break;
+			++run;
+			List<Integer> ini = chooseInitiators(nodes);
+			System.out.println("**************************************************************\nStarting new Round("+run+") with:");
+			//for(int j=0; j < ini.size();++j) {
+				System.out.print(nodes[2].name+"("+id[2]+") ");
+				System.out.print(nodes[5].name+"("+id[5]+") ");
+				System.out.print(nodes[7].name+"("+id[7]+") ");
+			//}
+			System.out.println();
+			//for(int j=0; j < ini.size();++j) {
+				nodes[2].setInitiator(id[2]);
+				nodes[5].setInitiator(id[5]);
+				nodes[7].setInitiator(id[7]);
+				new Thread(()->nodes[2].wakeup(null,id[2])).start();
+				new Thread(()->nodes[5].wakeup(null,id[5])).start();
+				new Thread(()->nodes[7].wakeup(null,id[7])).start();
+			//}
+		}
 	}
 	private static Integer[] createId(int length) {
 		Integer[] id  = new Integer[length];
@@ -37,6 +60,14 @@ public class Controller {
 		return id;
 
 	}
+	private static List<Integer> chooseInitiators(NodeInstance[] nodes) {
+		Random rng = new Random();
+		List<Integer> ini = new ArrayList<Integer>();
+		for(int i=0, numberOfInitiators = rng.nextInt(2)+2;i<numberOfInitiators;++i)
+			ini.add(rng.nextInt(nodes.length));
+		return ini;		
+	}
+	
 	private static NodeInstance[] createTestTree(int length, Integer[] id) {
 		NodeInstance[] nodes = new NodeInstance[length];
 		nodes[BLUE] = new NodeInstance("blue", false, id[0]);
@@ -52,11 +83,11 @@ public class Controller {
 		nodes[BLUE].setupNeighbours(nodes[MAGENTA],nodes[RED]);
 		nodes[MAGENTA].setupNeighbours(nodes[BLUE],nodes[GREEN]);
 		nodes[GREEN].setupNeighbours(nodes[MAGENTA],nodes[BROWN]);
-		nodes[RED].setupNeighbours(nodes[BLUE],nodes[YELLOW],nodes[ORANGE],nodes[RED]);
+		nodes[RED].setupNeighbours(nodes[BLUE],nodes[YELLOW],nodes[ORANGE]);
 		nodes[ORANGE].setupNeighbours(nodes[RED],nodes[BLACK],nodes[BROWN]);
 		nodes[YELLOW].setupNeighbours(nodes[RED],nodes[BLACK],nodes[WHITE]);
 		nodes[BLACK].setupNeighbours(nodes[ORANGE],nodes[YELLOW]);
-		nodes[WHITE].setupNeighbours(nodes[YELLOW],nodes[WHITE]);
+		nodes[WHITE].setupNeighbours(nodes[YELLOW]);
 		nodes[BROWN].setupNeighbours(nodes[ORANGE],nodes[GREEN]);
 		
 		return nodes;
@@ -65,10 +96,11 @@ public class Controller {
 	static void printChildren() {
 		if(fin) {
 			for(NodeInstance i: nodes) {
-				System.out.print("Children from "+i.toString()+": ");
-				for(NodeInstance x: i.children) {
-					System.out.print(x.toString()+" ");
-				}
+				//System.out.print(i.toString()+": "+i.maxId);
+				//System.out.print("Children from "+i.toString()+": ");
+				//for(NodeInstance x: i.children) {
+				//	System.out.print(x.toString()+" ");
+				//}
 				System.out.println();
 			}
 		}
--- parallelprog/src/pp_assignment3/election/NodeInstance.java
+++ parallelprog/src/pp_assignment3/election/NodeInstance.java
@@ -10,7 +10,7 @@ public class NodeInstance extends NodeAbstract implements Node {
     private int messages = 0;
     private Node father = null;
     public List<NodeInstance> children = new CopyOnWriteArrayList<NodeInstance>();
-    private int maxId = 0;
+    public int maxId = 0;
     
     public NodeInstance(String name, boolean initiator, int id) {
         super(name, initiator, id);
@@ -18,19 +18,97 @@ public class NodeInstance extends NodeAbstract implements Node {
 
     @Override
     public void hello(Node neighbour) {
-        if(!this.neighbours.contains(neighbour)  && neighbour != this) {
+        if(!this.neighbours.contains(neighbour) && neighbour != this) {
             this.neighbours.add(neighbour);
         }        
-    }
-    
+    }    
     
     @Override
-    public void wakeup(Node neighbour, int id) {
-        int tmp = messages;
-        if(!this.initiator) {
-            ++tmp;
-            ++messages;
+    public synchronized void wakeup(Node neighbour, int mid) {
+    	if(neighbour == null)				//starting wakeup
+    		for(Node i: neighbours)
+                    new Thread(()->i.wakeup(this,maxId)).start();
+    	else{
+    		if(maxId > mid)
+    			new Thread(()->neighbour.wakeup(this,maxId)).start();
+    		else if(maxId < mid) {
+    			messages = 1;
+    			father = neighbour;
+    			initiator = false;
+    			maxId = mid;    		
+    			for(Node i: neighbours)
+    				if(i != father)
+    					new Thread(()->i.wakeup(this,maxId)).start();
+    		}
+    		else if (maxId == mid)
+    			++messages;
+    		else
+    			System.out.println("Das darf nicht passieren.");
+    	
+    		if(father != null && messages == neighbours.size()) {
+    			new Thread(()->father.echo(this, maxId)).start();
+    		}
+    	}
+    }   	
+    	/*Wenn meineID > mid
+    		nachbar.wakeup(this,meineID)
+    	esle Wenn meineID < mid
+    		messages=1
+    		father = nachbarn
+    		meineId = mid
+    		wakeup an alle nachbarn außer father
+    	else Wenn meineID == mid
+    		messages+1
+    	
+    	Wenn messages==nachbar.size && father != null
+    		echo father.
+    	*/
+    	
+    	
+       /* int tmp = messages;
+        //if(!this.initiator) {
+        ++tmp;
+        ++messages;
+         if (mid > maxId) {
+        	System.out.println("Override "+maxId+" to "+mid+", "+neighbour.toString()+" becoming father of "+toString());
+        	father = neighbour;
+            maxId =  mid;
+            
+            messages = 1;
+        	if(initiator)
+        		System.out.println(toString()+" auf false mit id: "+mid);
+        	initiator = false;
+        	
+            if(!(tmp == this.neighbours.size())) {
+                for(Node i: neighbours) {
+                    if(i != father) {
+                    	System.out.println(toString()+"("+tmp+"), exploring: "+i.toString());
+                        new Thread(()->i.wakeup(this,maxId)).start();
+                    }
+                }
+            }
+            else if(tmp == this.neighbours.size())
+            	new Thread(()->father.echo(this, maxId)).start();
         }
+        else  if(father == null) {
+         	System.out.println(neighbour.toString()+" becoming father of "+toString());
+         	father = neighbour;
+            maxId = maxId < mid ? mid : maxId;
+             
+             if(!(tmp == this.neighbours.size())) {
+                 for(Node i: neighbours) {
+                     if(i != father) {
+                     	System.out.println(toString()+"("+tmp+"), exploring: "+i.toString());
+                         new Thread(()->i.wakeup(this,maxId)).start();
+                     }
+                 }
+             }
+             else if(tmp == this.neighbours.size())
+             	new Thread(()->father.echo(this, maxId)).start();
+        }
+        else if(tmp == this.neighbours.size())
+        	new Thread(()->father.echo(this, maxId)).start();
+        /*
         if(father == null || id > maxId) {            
             if(id > maxId) {
             	messages = 1;
@@ -42,39 +120,43 @@ public class NodeInstance extends NodeAbstract implements Node {
             
             father = neighbour;
             maxId = id;
-            //System.out.println(this.toString()+" added "+neighbour.toString()+" as father");
+            System.out.println(this.toString()+" added "+neighbour.toString()+" as father");
             if(!(tmp == this.neighbours.size())) {
                 for(Node i: neighbours) {
                     if(i != father) {
-                    	System.out.println(toString()+", exploring: "+i.toString());
+                    	System.out.println(toString()+"("+tmp+"), exploring: "+i.toString());
                         new Thread(()->i.wakeup(this,maxId)).start();
                     }
                 }
             }
-        }                
+        }
         if(tmp == this.neighbours.size())
         	new Thread(()->father.echo(this, maxId)).start();
-    }
+    }*/
     
     @Override
-    public void echo(Node neighbour, Object data) {
-    	if((int)data < maxId ) return;
+    public synchronized void echo(Node neighbour, Object data) {
+    	if((int)data < maxId )
+    		new Thread(()->neighbour.wakeup(this,maxId)).start();
         int tmp = ++messages;
         check.getAndIncrement();
-        System.out.println("Echo from: "+neighbour.toString()+" to: "+this.toString()+" "+tmp+" ini: "+initiator+ " size: "+neighbours.size());
+        //System.out.println("Echo from: "+neighbour.toString()+" to: "+this.toString()+" "+tmp+" ini: "+initiator+ " size: "+neighbours.size());
         //++messages;
-        children.add((NodeInstance) neighbour);
-        //System.out.println(this.toString()+": added "+neighbour.toString());
+        if(!children.contains((NodeInstance)neighbour) && (NodeInstance)neighbour != this) {
+        	children.add((NodeInstance) neighbour);
+        	//System.out.println(this.toString()+": added "+neighbour.toString());
+        }
         if(this.initiator && tmp == this.neighbours.size()) {    
             System.out.println("~fin~");
             Controller.printChildren();
             Controller.fin = false;
-//            System.out.println(this.toString());
-//            for(NodeInstance child : children) {
-//                System.out.print(child.toString()+" ");
-//                child.print();
-//                System.out.println();
-//            }
+            Controller.running = false;
+            System.out.println(this.toString());
+            for(NodeInstance child : children) {
+                System.out.print(child.toString()+" ");
+                child.print();
+                System.out.println();
+            }
             System.out.println("Echos: "+check.get());
             check.set(0);
         }    
